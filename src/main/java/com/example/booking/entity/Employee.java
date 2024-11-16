f5ec34c41dc65c7fd97ef5d3b18fb065ceff90f4
mfaherold1998 <mfaherold1998@gmail.com>
2024-11-16 19:04:14 +0100

    SonarCloud cicd analysis configured

--- src/main/java/com/example/booking/entity/Employee.java
+++ src/main/java/com/example/booking/entity/Employee.java
@@ -4,7 +4,6 @@ import com.example.booking.common.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
